af9f54a4f3b1b4ae171523d8511b5fc9fb3217a0
Naman Gupta <74008062+Naman-Gupta1234@users.noreply.github.com>
2021-07-12 22:51:33 +0530

    updates

--- js/index.js
+++ js/index.js
@@ -1,7 +1,14 @@
-var noOfDrums=document.querySelectorAll(".drum").length;
+var noOfDrums = document.querySelectorAll(".drum").length;
 
-for (var i=0;i<noOfDrums;i++){
-    document.querySelectorAll(".drum")[i].addEventListener("click",function() {
-        alert("I got Clicked!");
+// for (var i = 0; i < noOfDrums; i++) {
+//     document.querySelectorAll(".drum")[i].addEventListener("click", function () {
+//         alert("I got Clicked!");
+//     });
+// }
+for (var i = 0; i < noOfDrums; i++) {
+    document.querySelectorAll(".drum")[i].addEventListener("click", function () {
+        var audio = new Audio('sounds/tom-1.mp3');
+        audio.play();
+
     });
-}    
+}
